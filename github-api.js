ccbb4282032c2a3057325a05c2f6f7b11dfc1c4c
ProxyAyush <65772478+ProxyAyush@users.noreply.github.com>
2024-09-09 01:54:55 +0530

    Add files via upload
    again corb fixed

--- github-api.js
+++ github-api.js
@@ -1,12 +1,13 @@
 const script = document.createElement('script');
 script.src = "https://unpkg.com/@octokit/core@4/dist/index.js";
+script.crossOrigin = "anonymous";
 document.head.appendChild(script);
 
 script.onload = async () => {
 
     // GitHub API setup
 
-    const octokit = new Octokit.Octokit({
+    window.octokit = new Octokit.Octokit({
         baseUrl: 'https://api.github.com',
         accessToken: 'your-github-token'
     });
@@ -14,10 +15,10 @@ script.onload = async () => {
     const repoOwner = 'ProxyAyush';
     const repoName = 'eatcha'; 
 
-    const createIssue = async (userInfo) => {
+    window.createIssue = async (userInfo) => {
         // todo: validate the user info 
 
-        const newIssue = await octokit.issues.create({
+        const newIssue = await window.octokit.issues.create({
             owner: repoOwner,
             repo: repoName,
             title: 'New user info',
