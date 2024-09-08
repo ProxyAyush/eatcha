1df430f328017deff76c266c479c90c65cb0cfd2
ProxyAyush <65772478+ProxyAyush@users.noreply.github.com>
2024-09-09 01:38:36 +0530

    Add files via upload
    3rd commit

--- github-api.js
+++ github-api.js
@@ -1,14 +1,27 @@
-const GitHub = require('github-api');
+// Import the GitHub API
+const script = document.createElement('script');
+script.src = "https://unpkg.com/@octokit/octokit/dist/index.min.js";
+document.head.appendChild(script);
 
-const repoOwner = 'ProxyAyush';
-const repoName = 'eatcha';
+// Let's make sure the script is loaded before proceeding.
+script.onload = async () => {
 
-const github = new GitHub({
-  token: 'your-github-token',
-});
+    // GitHub API setup
+    const octokit = new Octokit.Octokit({
+        baseUrl: 'https://api.github.com',
+        accessToken: 'your-github-token'
+    });
 
-const repo = github.getRepo(repoOwner, repoName);
+    const repoOwner = 'ProxyAyush';
+    const repoName = 'eatcha'; 
 
-function createIssue(userInfo) {
-  repo.createIssue(userInfo, 'New user info', 'New user info submitted!')
+    const createIssue = async (userInfo) => {
+        // todo: validate the user info 
+        const newIssue = await octokit.rest.issues.create({
+            owner: repoOwner,
+            repo: repoName,
+            title: 'New user info',
+            body: `Here is the new user info: \n${JSON.stringify(userInfo)}`
+        });
+    }
 }
