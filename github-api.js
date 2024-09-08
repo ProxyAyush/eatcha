8de69459463beb1a2e7cc94a523f8b29fab05a92
ProxyAyush <65772478+ProxyAyush@users.noreply.github.com>
2024-09-09 03:13:16 +0530

    Add files via upload
    new file added

--- github-api.js
+++ github-api.js
@@ -1,19 +1,10 @@
-import { Octokit } from 'https://cdn.jsdelivr.net/npm/octokit@4.0.2/dist-src/index.js';
+class Octokit {
+    constructor(options) {
+        this.options = options;
+    }
 
-export async function createIssue(userInfo) {
-    // todo: validate the user info 
-
-    const octokit = new Octokit.Octokit({
-        baseUrl: 'https://api.github.com',
-        accessToken: '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'
-    });
-    const repoOwner = 'ProxyAyush';
-    const repoName = 'eatcha';
-
-    const newIssue = await octokit.issues.create({
-        owner: repoOwner,
-        repo: repoName,
-        title: 'New user info',
-        body: `Here is the new user info: \n${JSON.stringify(userInfo)}`
-    });
+    issuesCreate(options) {
+        // Use your logic to create an issue
+        console.log('Creating issue: ', options);
+    }
 }
