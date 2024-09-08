a55903f1b46d0881662dd22e2a699a457f192bab
ProxyAyush <65772478+ProxyAyush@users.noreply.github.com>
2024-09-09 03:35:37 +0530

    Add files via upload

--- github-api.js
+++ github-api.js
@@ -5,13 +5,22 @@ export class Octokit {
 
     issuesCreate(options) {
         // Use your logic to create an issue
-        console.log('Creating issue: ', options);
+        return fetch(`https://api.github.com/repos/${this.options.owner}/${this.options.repo}/issues`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${this.options.token}`, 
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(options),
+        }).then(response => response.json());
     }
 
     static getInstance() {
         return new Octokit({
             baseUrl: 'https://api.github.com',
-            accessToken: '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'
+            accessToken: 'your-github-token',
+            owner:'ProxyAyush',
+            repo:'eatcha'
         });
     }
 }
