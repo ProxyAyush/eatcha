455f1c0497b73ff71ef51e01921ccf9e830d3652
ProxyAyush <65772478+ProxyAyush@users.noreply.github.com>
2024-09-10 01:26:59 +0530

    Update github-api.js

--- github-api.js
+++ github-api.js
@@ -18,7 +18,7 @@ export class Octokit {
     static getInstance() {
         return new Octokit({
             baseUrl: 'https://api.github.com',
-            accessToken: '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',
+            accessToken: 'your-git-key',
             owner:'ProxyAyush',
             repo:'eatcha'
         });
